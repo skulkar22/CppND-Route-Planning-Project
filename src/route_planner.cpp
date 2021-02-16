dee3b5096c6e37efb150df04e13e38692e5e3f9b
Shubhankar <skulkar22@gmail.com>
2021-02-15 22:14:05 -0600

    minor change

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -13,10 +13,10 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     start_node = &m_Model.FindClosestNode(start_x, start_y);
     end_node = &m_Model.FindClosestNode(end_x, end_y);
 
-    std::cout << "Start node x: " << start_node->x << std::endl;
-    std::cout << "Start node y: " << start_node->y << std::endl;
-    std::cout << "End node x: " << end_node->x << std::endl;
-    std::cout << "End node y: " << end_node->y << std::endl;
+    //std::cout << "Start node x: " << start_node->x << std::endl;
+    //std::cout << "Start node y: " << start_node->y << std::endl;
+    //std::cout << "End node x: " << end_node->x << std::endl;
+    //std::cout << "End node y: " << end_node->y << std::endl;
 
 }
 
